1d7567ac1c2cca594c9cc4570e5d22cd631cf27b
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2023-10-11 14:27:15 +0700

    feat: add logout

--- includes/a_config.php
+++ includes/a_config.php
@@ -1,13 +1,4 @@
 <?php
-	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/kyb-skill-map-man-power/index.php":
-			$CURRENT_PAGE = "Dashboard"; 
-			$PAGE_TITLE = "Skill Map Man Power Dashboard";
-			break;
-			default:
-			$CURRENT_PAGE = "Index";
-			$PAGE_TITLE = "Welcome to my homepage!";
-		}
 	$PAGE_TITLE = "Skill Map Man Power Dashboard";
 	$members_per_pages = 4;
 	// keystring -> display name
--- includes/design-top.php
+++ includes/design-top.php
@@ -6,6 +6,9 @@
 		<a href="index.php" class='mr-4 font-default-light'>
 			Home
 		</a>
+		<a href="actions/logout.php" class='mr-4 font-default-light'>
+			Logout
+		</a>
 		<a href="index.php">
 			<img src="img/kyb-putih.png" alt="Skill Map">
 		</a>
