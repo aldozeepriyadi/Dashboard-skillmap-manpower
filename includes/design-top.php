3d0904801562e7a6d3552f1c1bad988187ebc65a
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2023-10-11 09:23:45 +0700

    feat: add home button for page header

--- includes/design-top.php
+++ includes/design-top.php
@@ -3,6 +3,9 @@
 		<p class="h1">SKILL MAP FOR MAN POWER</p>
 	</div>
 	<div class="flex-float-right logo-container">
+		<a href="index.php" class='mr-4 font-default-light'>
+			Home
+		</a>
 		<a href="index.php">
 			<img src="img/kyb-putih.png" alt="Skill Map">
 		</a>
