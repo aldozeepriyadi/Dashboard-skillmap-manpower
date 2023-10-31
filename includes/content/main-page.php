6227cf4488024605d052c5a745cf8e4b2f092b75
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2023-10-31 09:40:43 +0700

    feat: optimize main page query

--- includes/content/main-page.php
+++ includes/content/main-page.php
@@ -5,66 +5,64 @@
                 var ctx_list = {}
             </script>
             <?php
-            $q_res = $conn->query("SELECT * FROM department");
-            while ($dept_row = $q_res->fetch_assoc()) {
-                $dept = $dept_row['dept_name'];
-                $d_id = $dept_row['id'];
-                $chart_id = str_replace(" ", "-", $dept);
-                $operator_mp_categories = array(
-                    "msk"=>0.0,
-                    "kt"=>0.0,
-                    "pssp"=>0.0,
-                    "png"=>0.0,
-                    "fivejq"=>0.0
-                );
-                foreach($operator_mp_categories as $cat => $val) {
-                    $res = $conn->query("
-                        SELECT AVG(IFNULL(mp_scores.$cat,0)) as average
-                        FROM karyawan
-                        LEFT JOIN mp_scores ON karyawan.npk = mp_scores.npk
-                        WHERE karyawan.npk in (
-                            SELECT karyawan_workstation.npk FROM karyawan_workstation
-                            LEFT JOIN sub_workstations ON karyawan_workstation.workstation_id = sub_workstations.id
-                            LEFT JOIN workstations ON sub_workstations.workstation_id = workstations.id
-                            WHERE workstations.dept_id = $d_id
-                        )
-                    ");
-                    $row = $res->fetch_assoc();
-                    $avg_val = $row['average'];
-                    $operator_mp_categories[$cat] = $avg_val;
+            $q_res = $conn->query("SELECT 
+            AVG(IFNULL(mp_scores.msk,0)) as msk,
+            AVG(IFNULL(mp_scores.kt,0)) as kt,
+            AVG(IFNULL(mp_scores.pssp,0)) as pssp,
+            AVG(IFNULL(mp_scores.png,0)) as png,
+            AVG(IFNULL(mp_scores.fivejq,0)) as fivejq,
+            workstations.dept_id
+            FROM (SELECT npk FROM karyawan) as k
+                LEFT JOIN mp_scores ON k.npk = mp_scores.npk
+                LEFT JOIN karyawan_workstation ON karyawan_workstation.npk = k.npk
+                LEFT JOIN sub_workstations ON karyawan_workstation.workstation_id = sub_workstations.id
+                LEFT JOIN workstations ON sub_workstations.workstation_id = workstations.id
+            GROUP BY workstations.dept_id;
+            ");
+
+            $operator_mp_categories = array();
+            $dept_ids = array(1, 2, 3, 4, 5);
+
+            while ($row = $q_res->fetch_assoc()) {
+                if (in_array($row['dept_id'], $dept_ids)) {
+                    $operator_mp_categories[$row['dept_id']] = $row;
                 }
-                $query_string = "
-                    SELECT AVG(IFNULL(mp_scores.kao,0)) as average
-                    FROM karyawan
-                    LEFT JOIN karyawan_workstation ON karyawan_workstation.npk = karyawan.npk
-                    LEFT JOIN sub_workstations ON karyawan_workstation.workstation_id = sub_workstations.id
-                    LEFT JOIN workstations ON sub_workstations.workstation_id = workstations.id
-                    LEFT JOIN mp_scores ON karyawan.npk = mp_scores.npk
-                    WHERE workstations.dept_id = $d_id AND (
-                ";
-                foreach($roles_with_kao as $role) {
-                    $query_string .= "role = $role OR ";
+            }
+
+            $q_res = $conn->query("SELECT
+            AVG(IFNULL(mp_scores.kao,0)) as kao,
+            workstations.dept_id
+            FROM (SELECT npk FROM karyawan WHERE karyawan.role != 2) as k
+            LEFT JOIN mp_scores ON k.npk = mp_scores.npk
+                LEFT JOIN karyawan_workstation ON karyawan_workstation.npk = k.npk
+                LEFT JOIN sub_workstations ON karyawan_workstation.workstation_id = sub_workstations.id
+                LEFT JOIN workstations ON sub_workstations.workstation_id = workstations.id
+            GROUP BY workstations.dept_id;");
+
+            while ($row = $q_res->fetch_assoc()) {
+                if (in_array($row['dept_id'], $dept_ids)) {
+                    $operator_mp_categories[$row['dept_id']]['kao'] = $row['kao'];
                 }
-                if (count($roles_with_kao) > 0) $query_string = substr($query_string, 0, -4);
-                $query_string .= ")";
-                $res = $conn->query($query_string);
-                $row = $res->fetch_assoc();
-                $avg_val = $row['average'];
-                $operator_mp_categories["kao"] = $avg_val;
+            }
+            $q_res = $conn->query('SELECT id, dept_name FROM department;');
+            while ($row = $q_res->fetch_assoc()) {
+                $d_id = $row['id'];
+                $d_name = $row['dept_name'];
+                $chart_id = "dept-stat-$d_id";
                 $labels = "['MSK', 'KT', 'PSSP', 'PNG', '5JQ', 'KAO']";
                 $datas = "[".
-                        $operator_mp_categories['msk'].",".
-                        $operator_mp_categories['kt'].",".
-                        $operator_mp_categories['pssp'].",".
-                        $operator_mp_categories['png'].",".
-                        $operator_mp_categories['fivejq'].",".
-                        $operator_mp_categories['kao'].
+                        $operator_mp_categories[$d_id]['msk'].",".
+                        $operator_mp_categories[$d_id]['kt'].",".
+                        $operator_mp_categories[$d_id]['pssp'].",".
+                        $operator_mp_categories[$d_id]['png'].",".
+                        $operator_mp_categories[$d_id]['fivejq'].",".
+                        $operator_mp_categories[$d_id]['kao'].
                         "]";
                 echo
                     "
                     <a class='dept-stat-container' href='department_workstations.php?q=$d_id'>
                         <div class='ds-title'>
-                            <p>$dept</p>
+                            <p>$d_name</p>
                         </div>
                         <div class='ds-content'>
                     ";
