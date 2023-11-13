571126a5b42855dd969c33d36075352a60e05f92
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2023-11-13 10:24:28 +0700

    feat: minor button changes

--- includes/design-top.php
+++ includes/design-top.php
@@ -6,10 +6,10 @@
 		<p class="h1 ml-4">SKILL MAP MAN POWER DASHBOARD</p>
 	</div>
 	<div class='d-flex flex-row align-items-center'>
-		<a href="index.php" class='mr-4 font-default-light'>
+		<a href="index.php" class='font-default-light hover-overlay-light p-2'>
 			Home
 		</a>
-		<a href="actions/logout.php" class='mr-4 font-default-light'>
+		<a href="actions/logout.php" class='font-default-light hover-overlay-light p-2'>
 			Logout
 		</a>
 		<div>
